7f097a34ec5c634c3b3cf89456f98f351c40ebcb
arun kumar <arun.devpro@gmail.com>
2025-02-04 20:27:11 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import app from "./app.js";
+import connectDB from "./configs/db.configs.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const { PORT: port = 8000 } = process.env;
+
+const startServer = async () => {
+  try {
+    // connect to db
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server started \nhttp://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+    process.exit(1);
+  }
+};
+
+startServer();
