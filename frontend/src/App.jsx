7919688267f94d2ecb53a6e1d8fa0f223ad42203
arun kumar <arun.devpro@gmail.com>
2025-02-08 17:56:58 +0530

    implemented toast provider context for error handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ProtectedRoute } from "./components/ProtectedRoute";
 
 // lazy imports
 const Home = lazy(() => import("./pages/Home/Home"));
@@ -10,16 +9,7 @@ const App = () => {
     <Suspense fallback={true}>
       <Router>
         <Routes>
-          <Route
-            path="/*"
-            element={
-              <ProtectedRoute>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                </Routes>
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </Suspense>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,8 @@
 const Home = () => {
-  return <div className="home-page"></div>;
+  return (
+    <div className="home-page">
+      <h1>Home</h1>
+    </div>
+  );
 };
 export default Home;
