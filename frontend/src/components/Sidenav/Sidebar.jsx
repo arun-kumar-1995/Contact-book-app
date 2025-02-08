3175e9a9fb7878451f12962acc6eb60c2620b302
arun kumar <arun.devpro@gmail.com>
2025-02-08 19:23:28 +0530

    sidebar component

--- frontend/src/components/Sidenav/Sidebar.jsx
+++ frontend/src/components/Sidenav/Sidebar.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
+import "./Sidebar.css";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
+      <div className="logo">
+        <h1>
+          <Link to="/">Phonebook</Link>
+        </h1>
+      </div>
       <nav className="navbar">
         <ul>
           <Link to="/contacts">Contacts</Link>
           <Link to="/upload-contacts">Upload Contacts</Link>
-          <Link to="/contact-detail">Contact Detail</Link>
         </ul>
       </nav>
     </div>
--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -2,9 +2,13 @@ import Sidebar from "../components/Sidenav/Sidebar";
 
 export const AppLayout = ({ children }) => {
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
       <Sidebar />
       <main>{children}</main>
-    </>
+    </div>
   );
 };
