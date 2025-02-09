d0f4b74d3d4e1f0a4262796e49da7e983c8b3fe7
arun kumar <arun.devpro@gmail.com>
2025-02-09 12:57:56 +0530

    upload contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,26 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-// lazy imports
+// Lazy imports
 const Home = lazy(() => import("./pages/Home/Home"));
 const ContactDetails = lazy(() =>
   import("./pages/ContactDetails/ContactDetails")
 );
-
 const UploadContact = lazy(() => import("./pages/UploadContact/UploadContact"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound/PageNotFound"));
 
 const App = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Router>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact-details/:id" element={<ContactDetails />} />
           <Route path="/upload-contacts" element={<UploadContact />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-      </Router>
-    </Suspense>
+      </Suspense>
+    </Router>
   );
 };
 
--- frontend/src/pages/UploadContact/UploadContact.jsx
+++ frontend/src/pages/UploadContact/UploadContact.jsx
@@ -1,10 +1,24 @@
 import { Layout } from "../../hoc/Layout";
+import { FaRegFileAlt } from "react-icons/fa";
+import "./UploadContact.css";
 
 const UploadContact = () => {
   return (
-    <>
-      <h1>Upload Contact</h1>
-    </>
+    <div className="upload-contact-container">
+      <h2>Upload Contact</h2>
+      <div className="upload-wrapper">
+        <div className="upload-contact">
+          <FaRegFileAlt />
+          <div className="upload-info">
+            <p>Supports only csv file</p>
+          </div>
+          <div className="browse-file">
+            <input type="file" id="file-upload" />
+            <label htmlFor="file-upload">Browse</label>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
